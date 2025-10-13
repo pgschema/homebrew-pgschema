5e1b9aa6f13c3f986a97a8fdfdb47275cd963a3c
Tianzhou <t@bytebase.com>
2025-10-13 22:37:34 +0800

    Downgrade pgschema version from 1.3.0 to 1.2.0
    manually use an old version

--- Formula/pgschema.rb
+++ Formula/pgschema.rb
@@ -2,7 +2,7 @@ class Pgschema < Formula
   desc "Terraform-style, declarative schema migration for Postgres"
   homepage "https://www.pgschema.com"
   url "https://github.com/pgschema/pgschema/releases/download/v1.3.0/pgschema-darwin-arm64"
-  version "1.3.0"
+  version "1.2.0"
   sha256 "REPLACE_WITH_ACTUAL_SHA256"
   license "Apache-2.0"
 
