91b597dd91c3cd5d7cdf6eeda00eec4defaec395
Sam Mosleh <41725025+sam-mosleh@users.noreply.github.com>
2025-11-18 13:26:58 +0400

    Fix url

--- Formula/pgschema.rb
+++ Formula/pgschema.rb
@@ -1,7 +1,7 @@
 class Pgschema < Formula
   desc "Terraform-style, declarative schema migration for Postgres"
   homepage "https://www.pgschema.com"
-  url "https://github.com/pgschema/pgschema/releases/download/v1.4.2/pgschema-darwin-arm64"
+  url "https://github.com/pgschema/pgschema/releases/download/v1.4.2/pgschema-1.4.2-darwin-arm64"
   version "1.4.2"
   sha256 "32708397c3f322b9bc90312ec829f9fd4dae80fe57ecc8bb6f6c2fd9f4697926"
   license "Apache-2.0"
