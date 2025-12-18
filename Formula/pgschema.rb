4b4e70bc056cf5b5186a73dc090a24853a01ba2e
Tianzhou <t@bytebase.com>
2025-12-18 10:09:43 -0800

    pgschema 1.5.1

--- Formula/pgschema.rb
+++ Formula/pgschema.rb
@@ -1,9 +1,9 @@
 class Pgschema < Formula
   desc "Terraform-style, declarative schema migration for Postgres"
   homepage "https://www.pgschema.com"
-  url "https://github.com/pgschema/pgschema/releases/download/v1.5.0/pgschema-1.5.0-darwin-arm64"
-  version "1.5.0"
-  sha256 "f862dfce6de50ca0309b69d894c0d88a4efbaaaec5f1ff8382d9718c6e88191a"
+  url "https://github.com/pgschema/pgschema/releases/download/v1.5.1/pgschema-1.5.1-darwin-arm64"
+  version "1.5.1"
+  sha256 "976e2db6f65c79e0b4e97ae9425a784f9798f9ab7c7bbe992ef6534dfd9dd003"
   license "Apache-2.0"
 
   def install
