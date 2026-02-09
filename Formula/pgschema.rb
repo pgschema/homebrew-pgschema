ad3f9b3c0bc9ac41fc8f758ab90953e13e665f7b
tianzhou <t@bytebase.com>
2026-02-08 20:22:20 -0800

    change pgschema to pgplex

--- Formula/pgschema.rb
+++ Formula/pgschema.rb
@@ -1,7 +1,7 @@
 class Pgschema < Formula
   desc "Terraform-style, declarative schema migration for Postgres"
   homepage "https://www.pgschema.com"
-  url "https://github.com/pgschema/pgschema/releases/download/v1.6.2/pgschema-1.6.2-darwin-arm64"
+  url "https://github.com/pgplex/pgschema/releases/download/v1.6.2/pgschema-1.6.2-darwin-arm64"
   version "1.6.2"
   sha256 "0d489d39493728ba96fd96fe8397758265b19ff23badf136f2aa96fa5694ee85"
   license "Apache-2.0"
