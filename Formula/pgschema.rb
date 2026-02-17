a82d1d905dcac6be314ff1077d420a7256a28cd2
Tianzhou <t@bytebase.com>
2026-02-16 21:29:56 -0800

    pgschema 1.7.1

--- Formula/pgschema.rb
+++ Formula/pgschema.rb
@@ -1,9 +1,9 @@
 class Pgschema < Formula
   desc "Terraform-style, declarative schema migration for Postgres"
   homepage "https://www.pgschema.com"
-  url "https://github.com/pgplex/pgschema/releases/download/v1.7.0/pgschema-1.7.0-darwin-arm64"
-  version "1.7.0"
-  sha256 "73ededb92fbb152155a161d7e514167280e9f8043e5529b74d79010c9703bbf3"
+  url "https://github.com/pgplex/pgschema/releases/download/v1.7.1/pgschema-1.7.1-darwin-arm64"
+  version "1.7.1"
+  sha256 "59fcee7c651b6a2591cea3f885dc3d834451546ba9cf4c607c07fe9e2f06051f"
   license "Apache-2.0"
 
   def install
